9f6b8561a6fdd5b1a79f36a2397a88641ce222c9
Andrew Sheelan Paliyan <andrewsheelan@yahoo.com>
2013-12-16 15:41:16 -0500

    Bible read

--- app/models/bible_verse.rb
+++ app/models/bible_verse.rb
@@ -2,5 +2,5 @@ class BibleVerse < ActiveRecord::Base
   belongs_to :language
   belongs_to :version
   belongs_to :book
-  attr_accessible :chapter, :verse, :language_id, :version_id, :book_id, :verse_text
+  attr_accessible :chapter, :verse, :language_id, :version_id, :book_id, :verse_text, :misc_data
 end
--- lib/sn_snv.rb
+++ lib/sn_snv.rb
@@ -7,7 +7,7 @@ page = form.submit form.buttons.first
 
 doc = page.parser
 
-
+puts "Successfully Parsed!"
 language = Language.find_by_ref('SN')
 version = Version.find_by_short_name('SNV')
 
@@ -17,6 +17,7 @@ page_books.each_with_index do |page_book, book_index|
   form.b = page_book.attr('value')
   page = form.submit
   doc = page.parser
+  puts "Parsed.." + page_book.text
 
   page_chapters = doc.css('#chapter option')
   page_chapters.each_with_index do |page_chapter, chapter_index|
@@ -24,14 +25,16 @@ page_books.each_with_index do |page_book, book_index|
     form.c=page_chapter.attr('value')
     page = form.submit
     doc = page.parser
+
+    puts "Parsed.." + page_book.text + "" + page_chapter.text
     page_verses = doc.css('.zef_bible_Chapter .zef_verse')
     page_verse_numbers = doc.css('.zef_bible_Chapter .zef_verse_number')
     page_verses.each_with_index do |page_verse, verse_index|
       p_verse = page_verse.text.strip
-      p_number = page_verse_numbers[verse_number].text.strip
-      BibleVerse.create(language: language.id, 
-                        version: version, 
-                        book: Book.find(book_index), 
+      p_number = page_verse_numbers[verse_index].text.strip
+      BibleVerse.create(language_id: language.id,
+                        version_id: version.id,
+                        book_id: Book.find(book_index + 1).id,
                         chapter: page_chapter.text.strip,
                         verse: p_number,
                         misc_data: p_verse,
